4d90821ad357158dd114b041dcb6f5829a587ccf
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-09-09 18:51:44 -0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import *
+if _name_ == "_main_":
+    from sys import argv
     from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>\n')
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
-        print('Unknown operator. Available operators: +, -, * and /')
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     a = int(argv[1])
     b = int(argv[3])
-    print('{:d} {} {:d} = {}'.format(a, argv[2], b,
-                                     add(a, b) if argv[2] == '+' else
-                                     sub(a, b) if argv[2] == '-' else
-                                     mul(a, b) if argv[2] == '*' else
-                                     div(a, b)))
+    print("{:d} {} {:d} = {:d}".format(a, argv[2], b,
+                                       add(a, b) if argv[2] == '+' else
+                                       sub(a, b) if argv[2] == '-' else
+                                       mul(a, b) if argv[2] == '*' else
+                                       div(a, b)))
