519d99dfdcd2b619e3f6c14ae5de3fddab4d8974
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-09-09 13:56:00 -0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from sys import *
     argc = len(argv) - 1
     print('{:d} {}{}'.format(argc, 'argument'if argc == 1 else 'arguments',
-         ':' if argc else '.'))
+                             ':' if argc else '.'))
     if argc:
-        for i in range (argc):
+        for i in range(argc):
             print('{:d}: {}'.format(i + 1, argv[i + 1]))
-
+            
