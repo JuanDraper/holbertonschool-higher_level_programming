9dbf9966fd35e8a9e08db43381887d4c16fe2702
juandraper <e146613@gmail.com>
2021-09-28 15:28:41 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+    blabllablalblalbal
+    return {}
+"""
+
+
+
+
 class Square:
-    """Square class"""
+    """
+    blablalbla
+    """
 
     def __init__(self, size):
         self.__size = size
