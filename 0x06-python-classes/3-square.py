c95d6fe40d87201d6688adfddda3a4627773fcfa
juandraper <e146613@gmail.com>
2021-09-28 15:47:46 -0700

    -3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""bllbablal
+"""
+
+
+class Square:
+    """Square class
+      Defining a square
+       return size
+    """
+    def __init__(self, size=0):
+        """blablalbla
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """blalbab"""
+        return self.__size ** 2
