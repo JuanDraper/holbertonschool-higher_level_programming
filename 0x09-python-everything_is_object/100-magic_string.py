a6e24b9421f380d82a23596224031aa4ec914189
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-10-05 20:05:57 -0300

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    vars(magic_string).setdefault('my', []).append("BestSchool")
-    return ", ".join(magic_string.my)
+def magic_string(M=[]):
+    M += ["BestSchool"]
+    return (", ".join(M))
