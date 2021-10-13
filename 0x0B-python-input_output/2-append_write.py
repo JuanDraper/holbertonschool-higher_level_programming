17a16de0c66bacef80b59a081f0480d0a724ebad
juandraper <e146613@gmail.com>
2021-10-13 16:11:26 -0700

    fixing

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ module containing append_write.
 
 def append_write(filename="", text=""):
     """
-    Appends to a file 
+    Appends to a file
     """
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
