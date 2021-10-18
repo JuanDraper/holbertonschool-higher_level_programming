f971c542d56e5fbcac74fb308805b892dc2e44f9
juandraper <e146613@gmail.com>
2021-10-18 12:21:05 -0700

    adding

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module containing the Base"""
-
+import json
 class Base:
     """
     Base class 
@@ -16,4 +16,44 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Json of a list of dictionarries"""
+        if list_dictionaries is None or list_dictionaries  == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ saves the json into a file"""
+        with open(cls.__name__ + ".json", "w") as f:
+            if list_objs is None or list_objs == []:
+                f.write("[]")
+                return
+            listi = list()
+            for i in list_objs:
+                listi.append(i.to_dictionary())
+            f.write(Base.to_json_string(listi))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns object from json"""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns a new object from a dictionary"""
+        if cls.__name__ == "Rectangle":
+            o = cls(1, 1)
+        elif cls.__name__ == "Square":
+                o = cls(1)
+        o.update(**dictionary)
+        return o
+
+    @classmethod
+    def load_from_file(cls):
+
 
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""module containing the rectangle"""
+"""
+This module contains the class Rectangle.
+"""
 from models.base import Base
 
-class Rectangle(Base):
-    """ the rectangle class"""
 
+class Rectangle(Base):
+    """Class rectangle."""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor """
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if type(height) is not int:
@@ -28,12 +31,9 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
-
-    @property
+    @property		
     def width(self):
-        """
-        width.
-        """
+        """Width of the rectangle"""
         return self.__width
 
     @width.setter
@@ -46,9 +46,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """
-        Height of the rectangle.
-        """
+        """Height of the rectangle"""
         return self.__height
 
     @height.setter
@@ -58,16 +56,87 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
+    @property
+    def x(self):
+        """
+        X coordinate of the rectangle.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+  
+    
+    @property
+    def y(self):
+        """Y coordinate of the rectangle.    """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
     
     def area(self):
-        """ returns the area of the rectangle"""
+       """Function that returns the area of a rectangle"""
+       return self.__width * self.__height
 
-        return self.__width * self.__height
-	
-	
+
     def display(self):
-        """Function that displays a rectangle using #
-        """
+        """Function that displays a rectangle using # """
         y_pos = "\n" * self.__y
         rect = (" " * self.__x + "#" * self.__width + "\n") * self.__height
         print(y_pos + rect[:-1])
+
+    def __str__(self):
+        """
+        String representation of the rectangle.
+        """
+        return "[Rectangle] (" + str(self.id) + ") " + str(self.__x) + "/" +\
+                str(self.__y) + " - " + str(self.__width) + "/" +\
+                str(self.__height)
+
+    def update(self, *args, **kwargs):
+        """
+        Function to update a rectangle.
+        """
+        if args != ():
+            try:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[2]
+                self.x = args[3]
+                self.y = args[4]
+            except Exception:
+                pass
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+
+    def to_dictionary(self):
+        """
+        Function that returns the dictionary representation of a rectangle.
+        """
+        new = dict()
+        for key in self.__dict__.keys():
+            new[key.split("_")[-1]] = self.__dict__[key]
+        return new 
