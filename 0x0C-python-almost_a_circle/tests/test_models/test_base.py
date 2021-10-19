523d02bd3c9d4b33084e96ee4c2387af4dff17f7
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-10-19 08:36:01 -0300

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,73 +1,121 @@
+
 #!/usr/bin/python3
 """
-Unittest for the class base.
+Contains tests for Base class
 """
+
 import unittest
-import os
-from models.base import Base
-from models.rectangle import Rectangle
+import inspect
+import pep8
+import json
+from models import base
+Base = base.Base
+
+
+class TestBaseDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Base class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
+
+    def test_pep8_conformance_base(self):
+        """Test that models/base.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_base(self):
+        """Test that tests/test_models/test_base.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_docstring(self):
+        """Tests for the module docstring"""
+        self.assertTrue(len(base.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Tests for the Base class docstring"""
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions"""
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
 
 
 class TestBase(unittest.TestCase):
-    """testing"""
+    """Tests to check functionality of Base class"""
+    def test_too_many_args(self):
+        """test too many args to init"""
+        with self.assertRaises(TypeError):
+            b = Base(1, 1)
 
-    def test_empty(self):
-        """empty instantiation. """
+    def test_no_id(self):
+        """Tests id as None"""
         b = Base()
         self.assertEqual(b.id, 1)
 
-    def test_more_arguments(self):
-        """instantiation with 1 more argument. """
-        with self.assertRaises(TypeError):
-            s = Base(1, 1)
+    def test_id_set(self):
+        """Tests id as not None"""
+        b98 = Base(98)
+        self.assertEqual(b98.id, 98)
 
-    def test_give_id(self):
-        """instance with a specific id. """
-        b = Base(500)
-        self.assertEqual(b.id, 500)
+    def test_no_id_after_set(self):
+        """Tests id as None after not None"""
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
 
-    def test_to_json_no_args(self):
-        """to_json_string without args. """
-        r = Rectangle(1, 1)
-        with self.assertRaises(TypeError):
-            r.to_json_string()
+    def test_nb_private(self):
+        """Tests nb_objects as a private instance attribute"""
+        b = Base(3)
+        with self.assertRaises(AttributeError):
+            print(b.nb_objects)
+        with self.assertRaises(AttributeError):
+            print(b.__nb_objects)
 
-    def test_to_json_more_args(self):
-        """to_json_string with more args. """
-        r = Rectangle(1, 1)
-        with self.assertRaises(TypeError):
-            r.to_json_string([], [])
+    def test_to_json_string(self):
+        """Tests regular to json string"""
+        Base._Base__nb_objects = 0
+        d1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
+        d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
+        json_s = Base.to_json_string([d1, d2])
+        self.assertTrue(type(json_s) is str)
+        d = json.loads(json_s)
+        self.assertEqual(d, [d1, d2])
 
-    def test_to_json_empty(self):
-        """to_json_string with empty list and None. """
-        r = Rectangle(1, 1)
-        self.assertEqual(r.to_json_string([]), "[]")
-        self.assertEqual(r.to_json_string(None), "[]")
+    def test_empty_to_json_string(self):
+        """Test for passing empty list/ None"""
+        json_s = Base.to_json_string([])
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
 
-    def test_save_to_file_no_args(self):
-        """save_to_file with no arguments. """
-        r = Rectangle(1, 1)
-        with self.assertRaises(TypeError):
-            r.save_to_file()
+    def test_None_to_json_String(self):
+        json_s = Base.to_json_string(None)
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
 
-    def test_save_to_file_more_args(self):
-        """save_to_file with more arguments. """
-        r = Rectangle(1, 1)
-        with self.assertRaises(TypeError):
-            r.save_to_file([], [])
+    def test_from_json_string(self):
+        """Tests regular from_json_string"""
+        json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
+{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
+        json_l = Base.from_json_string(json_str)
+        self.assertTrue(type(json_l) is list)
+        self.assertEqual(len(json_l), 2)
+        self.assertTrue(type(json_l[0]) is dict)
+        self.assertTrue(type(json_l[1]) is dict)
+        self.assertEqual(json_l[0],
+                         {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8})
+        self.assertEqual(json_l[1],
+                         {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
 
-    def test_save_to_file_empty(self):
-        """save_to_file with empty list and None. """
-        r = Rectangle(1, 1)
-        if os.path.isfile("Rectangle.json"):
-            os.remove("Rectangle.json")
-        r.save_to_file([])
-        self.assertTrue(os.path.isfile("Rectangle.json"))
+    def test_fjs_empty(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual([], Base.from_json_string(""))
 
-    def test_save_to_file_csv_empty(self):
-        """save_to_file_csv with empty list and None. """
-        r = Rectangle(1, 1)
-        if os.path.isfile("Rectangle.csv"):
-            os.remove("Rectangle.csv")
-        r.save_to_file_csv([])
-        self.assertTrue(os.path.isfile("Rectangle.csv"))
+    def test_fjs_None(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual([], Base.from_json_string(None))
