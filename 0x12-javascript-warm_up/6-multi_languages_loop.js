ae833e5a53f746cebf07570645e20a0e73ce6e5d
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-12-13 19:49:32 -0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* now with loops and arrays
 */
-const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 for (i; stringArray[i]; i++) {
   console.log(stringArray[i]);
