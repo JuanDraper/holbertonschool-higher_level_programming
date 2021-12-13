8a8c4727d4b60f2c6a55b2b2bd6e67085d4405c7
juandraper <e146613@gmail.com>
2021-12-13 16:12:30 +0000

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+let i = 0;
+if (!num) {
+  console.log('Missing size');
+} else {
+  for (i; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+}
