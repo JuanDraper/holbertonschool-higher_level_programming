64dc2baba84ab3c5832665fbde2909a6f2cc1fd1
juandraper <e146613@gmail.com>
2021-12-13 15:57:51 +0000

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* now with loops and arrays
+*/
+const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+for (i; i < stringArray.length; i++) {
+  console.log(stringArray[i]);
+}
